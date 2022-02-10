51482742c8ba98cc18fc18af866f1939b3b251e7
haguerrerob <haguerrerob@gmail.com>
2022-02-10 15:12:37 -0300

    Modif. app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,20 +1,22 @@
-import express from 'express'
-import morgan from 'morgan';
-import routes from './routes/index.routes';
+import express from "express";
+import morgan from "morgan";
+import routes from "./routes/index.routes";
 
 const server = express();
 
 // Middlewares...
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
-server.use(morgan('dev'));
-
+server.use(morgan("dev"));
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
 });
 
 server.use("/", routes);
@@ -25,6 +27,5 @@ server.use((err, req, res, next) => {
   console.error(err);
   res.status(status).send(message);
 });
-
 
 export default server;
